9575a4f97b07f0d9eabf4bf2c41805cb70a35b60
andreainzaghi <andreainzaghilavoro@gmail.com>
2022-05-12 12:58:25 +0200

    create obj

---
+++ createobj.js
@@ -0,0 +1,42 @@
+const contenitore = document.querySelector('#container');
+
+
+const Json = [
+
+    {
+        'id': 'square',
+        'height': 50,
+        'width': 50,
+        'borderRadius': 0,
+        'bgColor': 'red',
+
+    },
+    {
+        'id': 'circle',
+        'height': 50,
+        'width': 50,
+        'borderRadius': 50,
+        'bgColor': 'blue',
+
+    },
+];
+
+let Objectsdrag = (id, height, width, borderRadius, bgColor) => {
+
+    const obj = document.createElement('div');
+    obj.style.height = height + 'px';
+    obj.id = id;
+    obj.className = "target"
+    obj.style.width = width + 'px';
+    obj.style.borderRadius = borderRadius + '%';
+    obj.style.backgroundColor = bgColor;
+    obj.style.position = "absolute"
+    contenitore.appendChild(obj);
+}
+
+
+Json.forEach((element) => {
+
+    Objectsdrag(element.id, element.height, element.width, element.borderRadius, element.bgColor, element.marginTop);
+
+});
